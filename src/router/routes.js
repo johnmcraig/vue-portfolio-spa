0e744b71f414705bbb93fa1201cd811e9fc4762b
johnmcraig <jmcraig32884@gmail.com>
2019-12-29 17:05:33 -0500

    Refactored routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,23 +1,28 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-import Home from '@/components/pages/Home'
-import About from '@/components/pages/About'
-import Skills from '@/components/pages/Skills'
-import Portfolio from '@/components/pages/Portfolio'
+import Home from "@/components/pages/Home";
+import About from "@/components/pages/About";
+import Skills from "@/components/pages/Skills";
+import Portfolio from "@/components/pages/Portfolio";
 
+Vue.use(VueRouter);
 
-Vue.use(VueRouter)
-
-const routes = [
-    { path: '/', name: 'Home', component: Home},
-    { path: '/about', name: 'About', component: About},
-    { path: '/skills', name: 'Skills', component: Skills},
-    { path: '/portfolio', name: 'Portfolio', component: Portfolio},
-    { path: '*', component: Home},
-]
+// const routes = [
+//   { path: "/", name: "Home", component: Home },
+//   { path: "/about", name: "About", component: About },
+//   { path: "/skills", name: "Skills", component: Skills },
+//   { path: "/portfolio", name: "Portfolio", component: Portfolio },
+//   { path: "*", component: Home }
+// ];
 
 export default new VueRouter({
-    mode: 'history',
-    routes
-})
+  mode: "history",
+  routes: [
+    { path: "/", name: "Home", component: Home },
+    { path: "/about", name: "About", component: About },
+    { path: "/skills", name: "Skills", component: Skills },
+    { path: "/portfolio", name: "Portfolio", component: Portfolio },
+    { path: "*", component: Home }
+  ]
+});
