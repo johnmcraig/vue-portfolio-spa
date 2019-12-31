5b124fac22ee146b791c3b5a5c5d55a079837c8a
johnmcraig <jmcraig32884@gmail.com>
2019-12-31 00:16:53 -0500

    Removed image

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -4,7 +4,7 @@
       <div class="container">
         <div class="row">
           <div class="col-xl-12">
-            <img src="/" class="img-responsive img-circle" style="display:inline" alt="" />
+           
             <h1 class="display-4 mb-1">John M. Craig Jr.</h1>
             <span class="lead">
               <i class="fas fa-database"></i> Full-stack Web Developer |
@@ -23,15 +23,15 @@
           <p>Use the navigation links to find what you are looking for.</p>
           <p>
             I would suggest if you are looking for my list of projects to use the
-            <router-link to="/portfolio" class="btn btn-info btn-sm">Portfolio</router-link>page.
+            <router-link to="/portfolio" class="btn btn-info btn-sm">Portfolio</router-link> page.
           </p>
           <p>
             Or maybe the tools I use with the
-            <router-link to="/skills" class="btn btn-info btn-sm">Technology</router-link>page.
+            <router-link to="/skills" class="btn btn-info btn-sm">Technology</router-link> page.
           </p>
           <p>
             Or if you want to contact me for work or hire, go to the
-            <router-link to="/contact" class="btn btn-info btn-sm">Contact</router-link>page.
+            <router-link to="/contact" class="btn btn-info btn-sm">Contact</router-link> page.
           </p>
         </div>
       </div>
@@ -47,9 +47,13 @@ export default {
 </script>
 
 <style scoped>
+p {
+  text-align: center;
+}
 /* .container {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
   grid-template-rows: repeat(2, 100px);
+  <img src="/" class="img-responsive img-circle" style="display:inline" alt="" />
 } */
 </style>
