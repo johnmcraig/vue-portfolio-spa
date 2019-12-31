db2b3da0a390709a4a1333ee7e801675960618bd
johnmcraig <jmcraig32884@gmail.com>
2019-12-30 21:11:43 -0500

    Updated routes after rename

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 
 import Home from "@/components/pages/Home";
-import About from "@/components/pages/About";
+import Contact from "@/components/pages/Contact";
 import Skills from "@/components/pages/Skills";
 import Portfolio from "@/components/pages/Portfolio";
 
@@ -12,7 +12,7 @@ export default new VueRouter({
   mode: "history",
   routes: [
     { path: "/", name: "Home", component: Home },
-    { path: "/about", name: "About", component: About },
+    { path: "/contact", name: "Contact", component: Contact },
     { path: "/skills", name: "Skills", component: Skills },
     { path: "/portfolio", name: "Portfolio", component: Portfolio },
     { path: "*", component: Home }
