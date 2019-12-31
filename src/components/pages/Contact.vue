ba57aa5012769d9ec16a52ab587f72795d302432
johnmcraig <jmcraig32884@gmail.com>
2019-12-30 21:10:27 -0500

    Changed About to Contact

--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -4,8 +4,8 @@
       <div class="container">
         <div class="row">
           <div class="col-xl-12">
-            <h1 class="display-4 mb-1">The About &amp; Contact Info Page</h1>
-            <span class="lead"><i class="fas fa-file-signature"></i> Get to know about this site or contact me for work/info</span>
+            <h1 class="display-4 mb-1">Contact Info Page</h1>
+            <span class="lead"><i class="fas fa-file-signature"></i> View info about me for work or hire</span>
           </div>
         </div>
       </div>
@@ -15,7 +15,7 @@
       <div style="padding: 0.5rem;">
         <h3> <span class=""><i class="fas fa-id-card"></i></span> Contect Info</h3>
         <p><i class="fas fa-envelope"></i> Email: john.craig@ccalearn.tech</p>
-        <p><i class="fas fa-mobile-alt"></i> Phone: +1 (770) 466-0792 <em>Only accepting calls between 9:00am to 6:00pm EST (-05:30 GMT)</em></p>
+        <p><i class="fas fa-mobile-alt"></i> Phone: +1 (678) 245-9657 <em>Only accepting calls between 9:00am to 6:00pm EST (-05:30 GMT)</em></p>
         <p><i class="fab fa-linkedin"></i> LinkedIn: <a class="btn btn-sm btn-primary my-1 my-sm-0 card-link" href="https://www.linkedin.com/in/johnmcraigjr"><span class="fab fa-linkedin"></span> LinkedIn Profile</a></p>
         <p><i class="fas fa-file-code"></i> Website: <a class="btn btn-sm btn-primary my-1 my-sm-0 card-link" href="https://johnmcraig.github.io"><span class="fas fa-file-code"></span> Personal Website</a></p>
         
@@ -27,7 +27,7 @@
         <p>The following tools were used to create this portfolio site:</p>
         <ul>
           <li>Vue.js</li>
-          <li>BoostrapVue &amp; Boostrap 4.1</li>
+          <li>Boostrap 4.1 &amp; Fontawesome 5.12</li>
           <li>HTML 5</li>
           <li>CSS3</li>
           <li>Hosted on Github pages</li>
@@ -42,7 +42,7 @@
 
 <script>
 export default {
-  name: "About"
+  name: "Contact"
 };
 </script>
 
